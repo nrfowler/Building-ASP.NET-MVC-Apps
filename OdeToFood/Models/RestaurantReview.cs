a5211b3dd2aedabefa8ec522400fcdacb91062e4
Nathan Fowler <nrfowler@gmail.com>
2017-05-22 11:39:57 -0700

    Create a custom validation method using IValidateableObject
    Added a Validate method and made RestaurantReview implement IValidateableObject so that both the username cannot be Scott and rating less than 2.

--- OdeToFood/Models/RestaurantReview.cs
+++ OdeToFood/Models/RestaurantReview.cs
@@ -6,33 +6,11 @@ using System.Web;
 
 namespace OdeToFood.Models
 {
-    public class MaxWordsAttribute : ValidationAttribute
-    {
-        private int _maxWords;
-
-        public MaxWordsAttribute(int maxWords) : base("{0} has too many words")
-        {
-            _maxWords = maxWords;
-        }
-        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-        {
-            if (value != null)
-            {
-                var valueAsString = value.ToString();
-                if (valueAsString.Split(' ').Length > _maxWords)
-                {
-                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
-                    return new ValidationResult(errorMessage);
-
-                }
-            }
-            return ValidationResult.Success;
-        }
-    }
-    public class RestaurantReview
+    
+    public class RestaurantReview : IValidatableObject
     {
         public int Id { get; set; }   
-        [Range(1,10)]    
+        [Range(1,10, ErrorMessage ="custom")]    
         [Required]
         public int Rating { get; set; }
         [Required]
@@ -41,8 +19,14 @@ namespace OdeToFood.Models
         public string Body { get; set; }
         [Display(Name ="User name")]
         [DisplayFormat(NullDisplayText ="anonymous")]
-        [MaxWords(1)]
         public string ReviewerName { get; set; }
         public int RestaurantId { get; set; }
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Rating < 2 && ReviewerName.ToLower().StartsWith("scott"))
+            {
+                yield return new ValidationResult("Sorry, Scott, you can't do this");
+            }
+        }
     }
 }
