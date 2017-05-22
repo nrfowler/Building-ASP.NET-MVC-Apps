5f540b1f7d5869140af7ef43ab1b91b8c2da4d94
Nathan Fowler <nrfowler@gmail.com>
2017-05-22 11:31:29 -0700

    Add custom validation attribute to require username to be one word
    Created a subclass of ValidationAttribute called MaxWordsAttribute which is used by the username field of RestaurantReview.

--- OdeToFood/Models/RestaurantReview.cs
+++ OdeToFood/Models/RestaurantReview.cs
@@ -6,6 +6,29 @@ using System.Web;
 
 namespace OdeToFood.Models
 {
+    public class MaxWordsAttribute : ValidationAttribute
+    {
+        private int _maxWords;
+
+        public MaxWordsAttribute(int maxWords) : base("{0} has too many words")
+        {
+            _maxWords = maxWords;
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                var valueAsString = value.ToString();
+                if (valueAsString.Split(' ').Length > _maxWords)
+                {
+                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+                    return new ValidationResult(errorMessage);
+
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
     public class RestaurantReview
     {
         public int Id { get; set; }   
@@ -18,7 +41,7 @@ namespace OdeToFood.Models
         public string Body { get; set; }
         [Display(Name ="User name")]
         [DisplayFormat(NullDisplayText ="anonymous")]
-
+        [MaxWords(1)]
         public string ReviewerName { get; set; }
         public int RestaurantId { get; set; }
     }
