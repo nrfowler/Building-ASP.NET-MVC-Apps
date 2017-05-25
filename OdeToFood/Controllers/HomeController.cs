9b9ea4027aca15e53257583e642f28dd2e6f7505
Nathan Fowler <nrfowler@gmail.com>
2017-05-25 11:44:36 -0700

    Cache the result of the Index Action for 60 seconds

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -26,7 +26,7 @@ namespace OdeToFood.Controllers
             return Json(model, JsonRequestBehavior.AllowGet);
 
         }
-
+        [OutputCache(Duration = 60)]
         public ActionResult Index(string searchTerm = null, int page = 1)
         {
             var model =
