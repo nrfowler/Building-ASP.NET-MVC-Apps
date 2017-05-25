26a527454cde866005d378ba349bc10a7bbd1c4f
Nathan Fowler <nrfowler@gmail.com>
2017-05-25 13:58:14 -0700

    Use resources files to display English or French strings based on browser settings
    Created resources files and added accept language to the vary by header.

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -26,7 +26,7 @@ namespace OdeToFood.Controllers
             return Json(model, JsonRequestBehavior.AllowGet);
 
         }
-        [OutputCache(CacheProfile ="Long")]
+        [OutputCache(CacheProfile ="Long", VaryByHeader ="Accept-Language")]
         public ActionResult Index(string searchTerm = null, int page = 1)
         {
             var model =
